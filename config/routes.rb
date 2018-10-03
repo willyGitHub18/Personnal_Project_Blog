476b3e173eca1b4f0e63e69c04a80437c1118372
willy <wilg1569@gmail.com>
2018-10-03 15:31:08 +0200

    Sign up user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
 
   get 'posts/new-zealand', to: 'posts#index_nz', as: "new_zealand"
   get 'posts/thailand', to: 'posts#index_th', as: "thailand"
   get 'posts/laos', to: 'posts#index_la', as: "laos"
   get 'posts/vietnam', to: 'posts#index_vi', as: "vietnam"
+
+  resources :users
 
   resources :posts
 end
