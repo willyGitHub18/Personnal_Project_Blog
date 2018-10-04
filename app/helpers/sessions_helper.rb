968c3740d32b115adb12edda15da695deb6470e2
willy <wilg1569@gmail.com>
2018-10-04 14:45:42 +0200

    restrictions for not logged users and non admin

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,16 +4,16 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # Returns true if the given user is the current user.
-  def current_user?(user)
-    user == current_user
-  end
-  
   # Returns the current logged-in user (if any).
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
+  end
+
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
   end
 
   # Returns true if the user is logged in, false otherwise.
@@ -25,5 +25,14 @@ module SessionsHelper
   def log_out
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def is_admin?
+    if logged_in?
+      current_user.admin?
+      redirect_to root_path unless current_user.admin?
+    else
+      redirect_to root_path unless logged_in?
+    end
   end
 end
