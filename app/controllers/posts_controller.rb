4ac732c88e1e79bfd56a7f7d76037a9bdd39170e
willy <wilg1569@gmail.com>
2018-10-22 13:38:25 +0200

    Page index admin

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
     before_action :find_post, only: [:show, :edit, :update, :destroy]
-    before_action :is_admin?, only: [:new, :create, :edit, :update, :destroy]
+    before_action :is_admin?, only: [:new, :create, :index, :edit, :update, :destroy]
 
     def index
       @posts = Post.all
