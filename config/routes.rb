d844ad5f4d7e9d8117d8fd8f275f7e635337dc5f
willy <wilg1569@gmail.com>
2018-10-22 17:44:50 +0200

    Ordre requêtes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :photos
-  get 'sessions/new'
   root 'static_pages#home'
 
   get 'posts/new-zealand', to: 'posts#index_nz', as: "new_zealand"
   get 'posts/thailand', to: 'posts#index_th', as: "thailand"
   get 'posts/laos', to: 'posts#index_la', as: "laos"
   get 'posts/vietnam', to: 'posts#index_vi', as: "vietnam"
+
+  get 'sessions/new'
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
@@ -15,4 +15,6 @@ Rails.application.routes.draw do
   resources :users
 
   resources :posts
+
+  resources :photos
 end
