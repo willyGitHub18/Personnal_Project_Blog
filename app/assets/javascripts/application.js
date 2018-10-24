59f803310b5759b17a670e8f4296040023cc52ae
willy <wilg1569@gmail.com>
2018-10-24 10:11:06 +0200

    AJout bootstrap sprockets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,8 @@
 //= require activestorage
 //= require jquery3
 //= require popper
-//= require bootstrap
 //= require bootstrap-sprockets
+//= require bootstrap
 //= require js/plugins/plugins
 //= require js/assan.custom
 //= require cubeportfolio/js/jquery.cubeportfolio.min
