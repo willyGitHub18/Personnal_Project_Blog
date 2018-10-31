bccbce8cfb99fc50310d49c7441c0e1280985405
willy <wilg1569@gmail.com>
2018-10-31 11:21:08 +0100

    Helper accès derniers articles depuis n'importe quelle page (utilisation méthode dans la view et pas variable

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,4 @@
 class StaticPagesController < ApplicationController
   def home
-    @posts = Post.last(3).reverse
   end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,5 @@
 module StaticPagesHelper
+  def latest_posts
+    @latest_posts = Post.last(3).reverse
+  end
 end
