fabb6a1ea5fe8bd0e0f8e506b524f856ac25d226
willy <wilg1569@gmail.com>
2018-11-08 12:16:35 +0100

    Activestorage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
     belongs_to :category, inverse_of: :posts
-    mount_uploader :image, PictureUploader
+    has_one_attached :image
     validates :title, :content, :category_id, :date, :place, presence: true
 end
