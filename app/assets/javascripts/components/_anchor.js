092eebad927253c2b6c7e740f333c6dd8a751a5f
willy <wilg1569@gmail.com>
2018-11-14 10:48:14 +0100

    Ancre pour scroll sur première section et changement titre

---
+++ app/assets/javascripts/components/_anchor.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#topanchor').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
