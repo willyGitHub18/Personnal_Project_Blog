32a8ee323c12ae8e608a666400d5644da17e861c
willy <wilg1569@gmail.com>
2018-11-14 15:04:29 +0100

    Ancre vers résumé milieu page et about us dans footer

--- app/assets/javascripts/components/_anchor.js
+++ app/assets/javascripts/components/_anchor.js
@@ -4,3 +4,10 @@ $(document).ready(function() {
       $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
     });
   });
+
+$(document).ready(function() {
+  $('#midanchor').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
