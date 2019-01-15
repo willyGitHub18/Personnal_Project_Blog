b5c39cce8cfa82b2e9571eb23232e9e204b2f8cb
willy <wilg1569@gmail.com>
2019-01-15 04:44:03 +0100

    Config Shrine production S3

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,10 +1,29 @@
 require "shrine"
-require "shrine/storage/file_system"
-
-Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent
-}
+if Rails.env.development?
+  require "shrine/storage/file_system"
+  Shrine.storages = {
+    cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
+    store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent
+  }
+elsif Rails.env.test?
+  require 'shrine/storage/memory'
+  Shrine.storages = {
+    cache: Shrine::Storage::Memory.new,
+    store: Shrine::Storage::Memory.new
+  }
+else
+  require "shrine/storage/s3"
+  s3_options = {
+    access_key_id:     Rails.application.secrets.s3_access_key_id,
+    secret_access_key: Rails.application.secrets.s3_secret_access_key,
+    region:            Rails.application.secrets.s3_region,
+    bucket:            Rails.application.secrets.s3_bucket
+  }
+  Shrine.storages = {
+    cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
+    store: Shrine::Storage::S3.new(prefix: "store", **s3_options)
+  }
+end
 
 Shrine.plugin :activerecord # or :sequel
 Shrine.plugin :cached_attachment_data # for retaining the cached file across form redisplays
